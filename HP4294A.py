213a69f5014502debf3da418093fcdecd81cd688
valeruozi <valeruozi@gmail.com>
2017-03-14 11:46:07 +0100

    Update HP4294A.py

--- HP4294A.py
+++ HP4294A.py
@@ -1,1 +1,36 @@
-first file
+""" 
+Created on Mon Mar 13 10:45:17 2017 
+ 
+@author: valeruozi 
+""" 
+ 
+class HP4294:
+    def __init_(self,GPIBAdress,SerialCom):
+        self.addr = GPIBAdress
+    def inizialize():
+    def trigger():
+    def check_errors():
+    def calibration():
+    def Measure (self,sweep):
+        self.sendCommand()
+    def send():
+
+class Measure(freq):
+    def __initt__:
+    def get_amplitude():
+    def get_phase():
+        
+class Sweep:
+    def __init__(self,a,b,npoints,delay):
+        self.center = a
+        self.range = b
+        self.start = a
+        self.stop = b
+        self.npoints = npoints
+        self.delay = delay
+    def get_sweep()
+        
+        
+impAnalyzer = HP4294A(17)
+sweep1 = Sweep(1000,10000,100,"log")
+Z1 = impAnalyze.Measure(sweep1)
